df395c937fc7317ec25e5456ba514cd448cb50fd
LongJinwen <204084802@qq.com>
2023-03-17 23:33:33 +0800

    feat: 新增引导

--- micro-react-app-01/src/view/home/index.tsx
+++ micro-react-app-01/src/view/home/index.tsx
@@ -1,11 +1,34 @@
 import './index.less'
 
 import { useEffect } from 'react'
-
+import { Modal } from 'antd'
+import { useMemoizedFn, useMount } from 'ahooks'
 export default () => {
   useEffect(() => {
     console.log('home page')
   }, [])
+
+  const LOCAL_GIT_TIP_KEY = 'GIT_TIP'
+
+  const showGitTis = useMemoizedFn(() => {
+    Modal.confirm({
+      title: '提示',
+      content: '如果对你有帮助，请到github给个star吧️',
+      okText: '现在去',
+      cancelText: '不在提示',
+      centered: true,
+      onOk() {
+        window.open('https://github.com/8696/micro-web-demo')
+        window.localStorage.setItem(LOCAL_GIT_TIP_KEY, '1')
+      },
+      onCancel() {
+        window.localStorage.setItem(LOCAL_GIT_TIP_KEY, '1')
+      }
+    })
+  })
+  useMount(() => {
+    window.localStorage.getItem(LOCAL_GIT_TIP_KEY) !== '1' && showGitTis()
+  })
 
   return (
     <div className='home'>
